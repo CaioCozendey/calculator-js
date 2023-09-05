9e79b5391f4ff0e1e28db52b9dc2c7f4603171ca
Caio Dias Cozendey <41480153+CaioCozendey@users.noreply.github.com>
2023-09-05 11:09:54 -0300

    change: operation ? "color"

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ import './Button.css';
 
 export default props => {
   let btnCalc = 'bg-zinc-200 border-none h-14 w-14 rounded cursor-default active:bg-zinc-400 '
-  btnCalc += props.operation ? 'bg-secondary' : ''
+  btnCalc += props.operation ? 'bg-[#64CCC5]' : ''
   btnCalc += props.double ? 'col-span-2 w-[7.75rem]' : ''
   btnCalc += props.triple ? 'col-span-3 w-[11.75rem]' : ''
   return (
